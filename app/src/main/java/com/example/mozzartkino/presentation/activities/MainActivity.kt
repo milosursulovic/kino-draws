6fcea74077956155f4899a2ccf4cb9a274158d4b
Misa <milosursulovic2696@gmail.com>
2022-11-11 17:18:24 +0100

    Add navigation

--- app/src/main/java/com/example/mozzartkino/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/mozzartkino/presentation/activities/MainActivity.kt
@@ -2,13 +2,34 @@ package com.example.mozzartkino.presentation.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.mozzartkino.R
+import com.example.mozzartkino.databinding.ActivityMainBinding
+import com.example.mozzartkino.presentation.view_models.KinoViewModel
+import com.example.mozzartkino.presentation.view_models.KinoViewModelFactory
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    @Inject
+    lateinit var factory: KinoViewModelFactory
+
+    lateinit var viewModel: KinoViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvDraws.setupWithNavController(navController)
+        viewModel = ViewModelProvider(this, factory)[KinoViewModel::class.java]
     }
 }
