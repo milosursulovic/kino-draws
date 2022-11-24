9ae307a3849bfe3bb39587197c8120cb3824a734
Misa <milosursulovic2696@gmail.com>
2022-11-24 15:34:22 +0100

    Fix timer invocation for left time to be optimized

--- app/src/main/java/com/example/mozzartkino/presentation/adapters/KinoAdapter.kt
+++ app/src/main/java/com/example/mozzartkino/presentation/adapters/KinoAdapter.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mozzartkino.databinding.DrawItemBinding
 import com.example.mozzartkino.domain.model.Draw
+import com.example.mozzartkino.presentation.util.ViewHolderTimer
 import com.example.mozzartkino.presentation.util.ViewHolderTimerImpl
 import com.example.mozzartkino.util.date.DateTimeUtils
 import kotlinx.coroutines.Job
@@ -18,7 +19,6 @@ import java.util.*
 
 class KinoAdapter(private val app: Application) : RecyclerView.Adapter<KinoAdapter.ViewHolder>(),
     DateTimeUtils {
-    private var job: Job? = null
     private val jobs = mutableMapOf<Int, Job>()
 
     private val callback = object : DiffUtil.ItemCallback<Draw>() {
@@ -53,40 +53,31 @@ class KinoAdapter(private val app: Application) : RecyclerView.Adapter<KinoAdapt
 
     override fun onViewAttachedToWindow(holder: ViewHolder) {
         super.onViewAttachedToWindow(holder)
-        jobs[holder.viewId]?.job?.start()
+        val drawTime = Date(differ.currentList[holder.adapterPosition].drawTime)
+        jobs[holder.adapterPosition] = ViewHolderTimerImpl().getJob(app, drawTime, holder.leftTime, this@KinoAdapter)
     }
 
     override fun onViewDetachedFromWindow(holder: ViewHolder) {
         super.onViewDetachedFromWindow(holder)
-        jobs[holder.viewId]?.job?.cancel()
+        jobs[holder.adapterPosition]?.cancel()
+        jobs.remove(holder.adapterPosition)
     }
 
     inner class ViewHolder(private val binding: DrawItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        private var leftTime: TextView? = null
-        private var drawTime: Date? = null
-        val viewId = binding.root.id
+        var leftTime = binding.leftTime
+
+        init {
+            binding.root.setOnClickListener {
+                onItemClickListener?.let {
+                    it(differ.currentList[adapterPosition])
+                }
+            }
+        }
 
         fun bind(draw: Draw) {
-            drawTime = Date(draw.drawTime)
             val time = convertLongToTime(false, draw.drawTime)
-
             binding.drawTime.text = time
-            leftTime = binding.leftTime
-
-            if (binding.root.hasOnClickListeners().not()) {
-                binding.root.setOnClickListener {
-                    onItemClickListener?.let {
-                        it(draw)
-                    }
-                }
-            }
-            if (jobs.containsKey(viewId).not()) {
-                job = ViewHolderTimerImpl().getJob(app, drawTime!!, leftTime!!, this@KinoAdapter)
-                job?.let { newJob ->
-                    jobs[viewId] = newJob
-                }
-            }
         }
     }
 
